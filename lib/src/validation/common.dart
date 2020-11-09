0ddf02905040fe544b36e3fb470e96a03c278560
Stijn Van Bael <stijn.vanbael@sweetmustard.be>
2020-11-09 17:50:57 +0100

    Validation

--- lib/src/validation/common.dart
+++ lib/src/validation/common.dart
@@ -1,31 +1,45 @@
 typedef PropertyGetter<E> = dynamic Function(E entity);
 
 abstract class PropertyValidator {
-  List<ValidationError> validate(dynamic entity, String propertyName, dynamic propertyValue);
+  List<ValidationError> validateProperty(dynamic entity, String propertyName, dynamic propertyValue);
 }
 
-class CompositePropertyValidator {
+class CompositePropertyValidator implements PropertyValidator {
   final String propertyName;
   final List<PropertyValidator> validators;
   final PropertyGetter getter;
 
   CompositePropertyValidator(this.propertyName, this.validators, this.getter);
 
-  List<ValidationError> validate(dynamic entity) =>
-      validators.expand((validator) => validator.validate(entity, propertyName, getter(entity))).toList();
+  @override
+  List<ValidationError> validateProperty(dynamic entity, [String parentProperty, dynamic propertyValue]) => validators
+      .expand((validator) => validator.validateProperty(
+            entity,
+            parentProperty != null ? '$parentProperty.$propertyName' : propertyName,
+            getter(entity),
+          ))
+      .toList();
 }
 
-class EntityValidator {
+class EntityValidator implements PropertyValidator {
   final List<CompositePropertyValidator> propertyValidators;
 
   EntityValidator(this.propertyValidators);
 
-  List<ValidationError> validate(dynamic entity) =>
-      propertyValidators.expand((validator) => validator.validate(entity)).toList();
+  @override
+  List<ValidationError> validateProperty(dynamic entity, String propertyName, dynamic propertyValue) =>
+      propertyValidators
+          .expand((validator) => validator.validateProperty(propertyValue, propertyName))
+          .toList();
+
+  List<ValidationError> validateEntity(dynamic entity) =>
+      propertyValidators.expand((validator) => validator.validateProperty(entity)).toList();
 }
 
 class ValidationError {
   final String key;
 
   ValidationError(this.key);
+
+  Map<String, dynamic> toJson() => {'key': key};
 }
--- lib/src/validation/required.dart
+++ lib/src/validation/required.dart
@@ -4,7 +4,7 @@ class RequiredValidator implements PropertyValidator {
   static final RequiredValidator instance = RequiredValidator();
 
   @override
-  List<ValidationError> validate(dynamic entity, String propertyName, dynamic propertyValue) {
+  List<ValidationError> validateProperty(dynamic entity, String propertyName, dynamic propertyValue) {
     if (propertyValue == null || (propertyValue is String && propertyValue.isEmpty)) {
       return [RequiredError(propertyName)];
     }
@@ -16,4 +16,13 @@ class RequiredError extends ValidationError {
   final String propertyName;
 
   RequiredError(this.propertyName) : super('required');
+
+  @override
+  String toString() => 'A value for $propertyName is required.';
+
+  Map<String, dynamic> toJson() => {
+        'key': key,
+        'propertyName': propertyName,
+        'message': toString(),
+      };
 }
